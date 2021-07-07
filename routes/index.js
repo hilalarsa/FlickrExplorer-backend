b3dc3fe60fb4655df5bc945d77d1e491c96177fe
Hilal Arsa <hilal.arsa@edgeprop.sg>
2021-07-07 20:37:25 +0700

    Add vercel deployment config

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   let result = await axios.get("https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1")
   res.json(result.data);
 });
