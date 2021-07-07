e04acc3f9911eb3a11a8e1a6fa0c31c227947d2d
Hilal Arsa <hilal.arsa@edgeprop.sg>
2021-07-08 01:16:33 +0700

    Remove vercel deployment config Add routes to search by tag Add .env for Flickr credentials

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,21 @@ var express = require('express');
 var axios = require('axios')
 var router = express.Router();
 
+FLICKR_API_KEY="758f0d0cdba6ce905341bcd6372e2fd0"
+FLICKR_SECRET="a68f13ffaeecbea1"
+
 /* GET home page. */
 router.get('/', async (req, res, next) => {
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   let result = await axios.get("https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1")
+  res.json(result.data);
+});
+
+router.get('/tags/:tag', async (req, res, next) => {
+  let {tag} = req.params
+  let result = await axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${FLICKR_API_KEY}&tags=${tag}&format=json&nojsoncallback=1`)
+  if(!tag){
+    res.status(500).json({status: "500"})
+  }
   res.json(result.data);
 });
 
